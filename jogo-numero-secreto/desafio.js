eb5f112b7f4c1653bc71ac2b8bede7201fb1161f
Bruno Roca <brunofroca01@gmail.com>
2024-11-27 22:49:14 -0300

    first commit

---
+++ jogo-numero-secreto/desafio.js
@@ -0,0 +1,11 @@
+Number(prompt( 'Insira o Número da Conta'))
+let numeroConta = 15020
+let saldoConta = ('R$ 21.000,00')
+if (numeroConta == 15020 ) {
+    alert (`Este é seu saldo em Conta Atual ${saldoConta}
+        `)
+} else {
+    alert ('Conta Incorreta! Tente novamente')
+}
+
+// teste pasta
